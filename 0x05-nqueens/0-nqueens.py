0808cc22181d41099b258f5460f6fbb0fac5e38a
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-08-03 09:30:03 -0400

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,43 +1,73 @@
 #!/usr/bin/python3
-""" Solves N Queen Problem """
-
+""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
 import sys
 
 
-def safety_check(Board, line, i):
-    """ Checking if can insert Quen at column
-    i in that line on Board """
-    for n in range(line):
-        if (Board[n] == i or
-                Board[n] + line - n == i or
-                Board[n] + n - line == i):
-            return False
-    return True
+class NQueen:
+    """ Class for solving N Queen Problem """
+
+    def __init__(self, n):
+        """ Global Variables """
+        self.n = n
+        self.x = [0 for i in range(n + 1)]
+        self.res = []
+
+    def place(self, k, i):
+        """ Checks if k Queen can be placed in i column (True)
+        or if the are attacking queens in row or diagonal (False)
+        """
+
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # There is already a queen in column
+            # or a queen in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """ Tries to place every queen in the board
+        Args:
+        k: starting queen from which to evaluate (should be 1)
+        """
+        # i goes from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # Placed all 4 Queens (A solution was found)
+                    solution = []
+                    for i in range(1, self.n + 1):
+                        solution.append([i - 1, self.x[i] - 1])
+                    self.res.append(solution)
+                else:
+                    # Need to place more Queens
+                    self.nQueen(k + 1)
+        return self.res
 
 
-def line_fill(Board, line):
-    """ Filling eaxh line of the board with
-    correct index """
-    for i in range(len(Board)):
-        if safety_check(Board, line, i):
-            Board[line] = i
-            if line < len(Board) - 1:
-                line_fill(Board, line + 1)
-            else:
-                print([[i, Board[i]] for i in range(len(Board))])
-
+# Main
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
+
+N = sys.argv[1]
+
 try:
-    n = int(sys.argv[1])
-except Exception:
+    N = int(N)
+except ValueError:
     print("N must be a number")
     sys.exit(1)
-if n < 4:
+
+if N < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-Board = [-1 for i in range(n)]
-line_fill(Board, 0)
+queen = NQueen(N)
+res = queen.nQueen(1)
+
+for i in res:
+    print(i)
