7d10d32cdbffa54470bc458ffdce437e0c09c4e5
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-08-04 07:24:07 -0400

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,73 +1,123 @@
 #!/usr/bin/python3
-""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
+'''
+N Queen Puzzle
+    Description:
+        The N queens puzzle is the challenge of
+        placing N non-attacking queens on an N×N chessboard.
+
+    Usage:
+        If the user called the program with the wrong
+        number of arguments, print Usage:
+        nqueens N, followed by a new line, and exit with the status 1
+        where N must be an integer greater or equal to 4
+        If N is not an integer, print N must be a number,
+        followed by a new line,
+        and exit with the status 1
+        If N is smaller than 4, print N must be at least 4,
+        followed by a new line,
+        and exit with the status 1
+        The program should print every possible solution to the problem
+        One solution per line
+        Format: see example
+        You don’t have to print the solutions in a specific order
+
+'''
+
+
 import sys
 
 
-class NQueen:
-    """ Class for solving N Queen Problem """
+if __name__ == "__main__":
 
-    def __init__(self, n):
-        """ Global Variables """
-        self.n = n
-        self.x = [0 for i in range(n + 1)]
-        self.res = []
+    '''
+    If the user called the program with the
+    wrong number of arguments, print Usage:
+    nqueens N, followed by a new line, and exit with the status 1
+    where N must be an integer greater or equal to 4
+    If N is not an integer, print N must be a number, followed by a new line,
+    and exit with the status 1
+    If N is smaller than 4, print N must be at least 4, followed by a new line,
+    and exit with the status 1
 
-    def place(self, k, i):
-        """ Checks if k Queen can be placed in i column (True)
-        or if the are attacking queens in row or diagonal (False)
-        """
+    '''
+    if not len(sys.argv) == 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-        # j checks from 1 to k - 1 (Up to previous queen)
-        for j in range(1, k):
-            # There is already a queen in column
-            # or a queen in same diagonal
-            if self.x[j] == i or \
-               abs(self.x[j] - i) == abs(j - k):
-                return 0
-        return 1
+    if not (sys.argv[1]).isdigit():
+        print("N must be a number")
+        sys.exit(1)
 
-    def nQueen(self, k):
-        """ Tries to place every queen in the board
-        Args:
-        k: starting queen from which to evaluate (should be 1)
-        """
-        # i goes from column 1 to column n (1st column is 1st index)
-        for i in range(1, self.n + 1):
-            if self.place(k, i):
-                # Queen can be placed in i column
-                self.x[k] = i
-                if k == self.n:
-                    # Placed all 4 Queens (A solution was found)
-                    solution = []
-                    for i in range(1, self.n + 1):
-                        solution.append([i - 1, self.x[i] - 1])
-                    self.res.append(solution)
-                else:
-                    # Need to place more Queens
-                    self.nQueen(k + 1)
-        return self.res
+    N = int(sys.argv[1])
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
 
-# Main
+def BoadOk(board, row, column, N):
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    '''
+    Checks if a queen can be placed on the board
+    Returns:
+        True if queen could be placed
+        False if there is not a save place
+    '''
 
-N = sys.argv[1]
+    for i in range(column):
+        if board[row][i] == 1:
+            return False
 
-try:
-    N = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    for i, j in zip(range(row, -1, -1),
+                    range(column, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    for i, j in zip(range(row, N, 1),
+                    range(column, -1, -1)):
+        if board[i][j] == 1:
+            return False
 
-queen = NQueen(N)
-res = queen.nQueen(1)
+    return True
 
-for i in res:
-    print(i)
+
+def SolutionNQ(board, column, N):
+
+    '''
+    solves the n queen problem using Backtracking
+    by finding the posibles board to placed all the n queens on it
+    in a save places
+    Returns:
+        True if all the queens are placed on the board
+        False if a queen can not be placed
+    '''
+
+    if column == N:
+        Board(board)
+        return True
+
+    f = False
+    for i in range(N):
+        if BoadOk(board, i, column, N):
+            board[i][column] = 1
+            f = SolutionNQ(board, column + 1, N) or f
+            board[i][column] = 0
+    return f
+
+
+def Board(board):
+
+    '''
+    prints the row and column where the queens are
+    positioned. "Our Board"
+    '''
+
+    solve = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solve.append([i, j])
+    print(solve)
+
+
+board = [[0 for i in range(N)] for j in range(N)]
+SolutionNQ(board, 0, N)
