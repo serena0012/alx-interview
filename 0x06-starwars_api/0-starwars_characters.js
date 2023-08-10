eeb0a87450e8e525c0cc0cd6cf4c9831cb6c5ce3
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-08-09 20:13:32 -0400

    updated

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,28 @@
 #!/usr/bin/node
+/* Prints all Casts of Star Wars movie
+Read the README.md file for more info
+*/
 
 const request = require('request');
+const starWarsAPI = 'https://swapi-api.alx-tools.com/api/';
+const endPoint = 'films/';
+const movieID = process.argv[2].toString();
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request.get(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film data:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error fetching film data. Status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-  const characterUrls = filmData.characters;
-
-  characterUrls.forEach((characterUrl) => {
-    request.get(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character data:', charError);
-        return;
-      }
-
-      if (charResponse.statusCode === 200) {
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
-      } else {
-        console.error('Error fetching character data. Status code:', charResponse.statusCode);
-      }
-    });
-  });
+request(starWarsAPI + endPoint + movieID, function (error, _, body) {
+  if (error) console.error(error);
+  const objects = JSON.parse(body);
+  const casts = objects.characters;
+  Printresult(casts);
 });
+
+/* reculsively and synchronously request for each character
+and prints out the casts */
+function Printresult (casts, counter = 0) {
+  request(casts[counter], function (error, _, body) {
+    if (error) console.error(error);
+    console.log(JSON.parse(body).name);
+    if (++counter < casts.length) {
+      Printresult(casts, counter++);
+    }
+  });
+}
